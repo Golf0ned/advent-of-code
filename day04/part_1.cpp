704cc6486ba5301726bee4530c04538d5e4f8f16
Golf0ned <benjaminye.email@gmail.com>
2024-12-05 01:51:54 -0600

    [d4] update with header files

--- day04/part_1.cpp
+++ day04/part_1.cpp
@@ -1,6 +1,7 @@
-#include <fstream>
 #include <iostream>
 #include <vector>
+
+#include "../utils/aoc_parser.h"
 
 int check_xmas(std::vector<std::vector<char>> input, int x, int y) {
     int res = 0;
@@ -38,26 +39,9 @@ int part_1(std::vector<std::vector<char>> input) {
     return res;
 }
 
-std::vector<std::vector<char>> parse_input(std::string file) {
-    std::ifstream inFile(file);
-    if (!inFile.is_open()) {
-        throw std::runtime_error(file + " not found");
-    }
-
-    std::vector<std::vector<char>> input;
-    std::string line;
-
-    while (std::getline(inFile, line)) {
-        input.emplace_back(line.begin(), line.end());
-    }
-
-    inFile.close();
-    return input;
-}
-
 int main(int argc, char* argv[]) {
     try {
-        std::vector<std::vector<char>> input = parse_input(argv[1]);
+        std::vector<std::vector<char>> input = parse_2d_char_vec(argv[1]);
         int res = part_1(input);
         std::cout << res << std::endl;
         return 0;
--- day04/part_2.cpp
+++ day04/part_2.cpp
@@ -1,6 +1,7 @@
-#include <fstream>
 #include <iostream>
 #include <vector>
+
+#include "../utils/aoc_parser.h"
 
 int check_xmas(std::vector<std::vector<char>> input, int x, int y) {
     int x_max = input.size();
@@ -37,26 +38,9 @@ int part_2(std::vector<std::vector<char>> input) {
     return res;
 }
 
-std::vector<std::vector<char>> parse_input(std::string file) {
-    std::ifstream inFile(file);
-    if (!inFile.is_open()) {
-        throw std::runtime_error(file + " not found");
-    }
-
-    std::vector<std::vector<char>> input;
-    std::string line;
-
-    while (std::getline(inFile, line)) {
-        input.emplace_back(line.begin(), line.end());
-    }
-
-    inFile.close();
-    return input;
-}
-
 int main(int argc, char* argv[]) {
     try {
-        std::vector<std::vector<char>> input = parse_input(argv[1]);
+        std::vector<std::vector<char>> input = parse_2d_char_vec(argv[1]);
         int res = part_2(input);
         std::cout << res << std::endl;
         return 0;
