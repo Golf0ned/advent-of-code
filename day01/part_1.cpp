45b697f41ff4a00f5466625287f8a5388b4ac222
Golf0ned <benjaminye.email@gmail.com>
2024-12-05 01:20:53 -0600

    [d1] update with header files

--- day01/part_1.cpp
+++ day01/part_1.cpp
@@ -1,10 +1,9 @@
 #include <algorithm>
 #include <cmath>
-#include <fstream>
 #include <iostream>
-#include <sstream>
-#include <stdexcept>
 #include <vector>
+
+#include "../utils/aoc_parser.h"
 
 int part_1(std::vector<std::vector<int>> input) {
     int res = 0;
@@ -21,32 +20,9 @@ int part_1(std::vector<std::vector<int>> input) {
     return res;
 }
 
-std::vector<std::vector<int>> parse_input(std::string file) {
-    std::ifstream inFile(file);
-    if (!inFile.is_open()) {
-        throw std::runtime_error(file + " not found");
-    }
-
-    std::string line;
-    std::vector<std::vector<int>> input(2, std::vector<int>());
-
-    while (std::getline(inFile, line)) {
-        std::istringstream stream(line);
-        int num1, num2;
-        if (stream >> num1 >> num2) {
-            input[0].push_back(num1);
-            input[1].push_back(num2);
-        }
-    }
-
-    inFile.close();
-
-    return input;
-}
-
 int main(int argc, char* argv[]) {
     try {
-        std::vector<std::vector<int>> input = parse_input(argv[1]);
+        std::vector<std::vector<int>> input = parse_2_int_cols(argv[1]);
         int res = part_1(input);
         std::cout << res << std::endl;
         return 0;
