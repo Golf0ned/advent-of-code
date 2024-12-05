34e77e07137bb25e572f008016541a2233ef076c
Golf0ned <benjaminye.email@gmail.com>
2024-12-05 01:50:15 -0600

    [utils][parser] restructured

--- utils/aoc_parser.cpp
+++ utils/aoc_parser.cpp
@@ -2,28 +2,11 @@
 #include <sstream>
 #include <vector>
 
-std::vector<std::vector<int>> parse_2_int_cols(std::string file) {
-    std::vector<std::vector<int>> input(2);
+//
+// General parsers
+//
 
-    std::ifstream inFile(file);
-    if (!inFile.is_open()) {
-        throw std::runtime_error(file + " not found");
-    }
-
-    std::string line;
-    while (std::getline(inFile, line)) {
-        std::istringstream stream(line);
-        int num1, num2;
-        if (stream >> num1 >> num2) {
-            input[0].push_back(num1);
-            input[1].push_back(num2);
-        }
-    }
-
-    inFile.close();
-    return input;
-}
-
+// 2d char vector
 std::vector<std::vector<char>> parse_2d_char_vec(std::string file) {
     std::vector<std::vector<char>> input;
 
@@ -41,6 +24,7 @@ std::vector<std::vector<char>> parse_2d_char_vec(std::string file) {
     return input;
 }
 
+// 2d int vector
 std::vector<std::vector<int>> parse_2d_int_vec(std::string file) {
     std::vector<std::vector<int>> input;
 
@@ -64,6 +48,7 @@ std::vector<std::vector<int>> parse_2d_int_vec(std::string file) {
     return input;
 }
 
+// string
 std::string parse_string(std::string file) {
     std::ifstream inFile(file);
     if (!inFile.is_open()) {
@@ -73,6 +58,33 @@ std::string parse_string(std::string file) {
     std::stringstream buffer;
     buffer << inFile.rdbuf();
     std::string input = buffer.str();
+
+    inFile.close();
+    return input;
+}
+
+//
+// Specific parsers
+//
+
+// 2 int columns (day 1)
+std::vector<std::vector<int>> parse_2_int_cols(std::string file) {
+    std::vector<std::vector<int>> input(2);
+
+    std::ifstream inFile(file);
+    if (!inFile.is_open()) {
+        throw std::runtime_error(file + " not found");
+    }
+
+    std::string line;
+    while (std::getline(inFile, line)) {
+        std::istringstream stream(line);
+        int num1, num2;
+        if (stream >> num1 >> num2) {
+            input[0].push_back(num1);
+            input[1].push_back(num2);
+        }
+    }
 
     inFile.close();
     return input;
--- utils/aoc_parser.h
+++ utils/aoc_parser.h
@@ -4,7 +4,10 @@
 #include <sstream>
 #include <vector>
 
-std::vector<std::vector<int>> parse_2_int_cols(std::string file);
+// General parsers
 std::vector<std::vector<char>> parse_2d_char_vec(std::string file);
 std::vector<std::vector<int>> parse_2d_int_vec(std::string file);
 std::string parse_string(std::string file);
+
+// Specific parsers
+std::vector<std::vector<int>> parse_2_int_cols(std::string file);
